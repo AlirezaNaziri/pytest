bd776898c918e3f5976416dfd71b23f7044f48be
Alireza Naziri Khansari <54983238+AlirezaNaziri@users.noreply.github.com>
2023-11-09 10:20:32 +0100

    adding function

--- test_myfunc.py
+++ test_myfunc.py
@@ -1,21 +1,33 @@
-import unittest
 from myfunc import add_numbers
 
-class TestAddNumbers(unittest.TestCase):
-    def test_add_positive_numbers(self):
-        result =add_numbers(3,5)
-        self.assertEqual(result,8)
 
-    def test_add_negative_numbers(self):
-        result =add_numbers(-3,-4)
-        self.assertEqual(result,-7)
 
-    def test_add_mixed_numbers(self):
-        result =add_numbers(3,-5)
-        self.assertEqual(result,-2)
+def test_add():
+    assert add_numbers(1, 2) == 3
+    assert add_numbers(-1, 1) == 0
+    assert add_numbers(0, 0) == 0
 
-if __name__ == '__main__':
-    unittest.main()
+
+
+
+
+
+# import unittest
+# class TestAddNumbers(unittest.TestCase):
+#     def test_add_positive_numbers(self):
+#         result =add_numbers(3,5)
+#         self.assertEqual(result,8)
+
+#     def test_add_negative_numbers(self):
+#         result =add_numbers(-3,-4)
+#         self.assertEqual(result,-7)
+
+#     def test_add_mixed_numbers(self):
+#         result =add_numbers(3,-5)
+#         self.assertEqual(result,-2)
+
+# if __name__ == '__main__':
+#     unittest.main()
 
     
 
