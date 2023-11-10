8ccab36b608959cb5ee4bcf3591e16333ad0e493
Alireza Naziri <nali@equinor.com>
2023-11-10 14:55:07 +0100

    add more functions

--- test_myfunc.py
+++ test_myfunc.py
@@ -1,13 +1,24 @@
-from myfunc import add_numbers
-
-
+from myfunc import add_numbers, subtract_numbers, multiply_numbers, divide_numbers
+import pytest
 
 def test_add():
     assert add_numbers(1, 2) == 3
     assert add_numbers(-1, 1) == 0
     assert add_numbers(0, 0) == 0
 
+@pytest.mark.parametrize("a, b, expected_result", [
+    (6, 2, 3),
+    (10, 2, 5),
+    (-8, -4, 2),
+    (1, 0.5, 2),
+])
+def test_division(a, b, expected_result):
+    result = divide_numbers(a, b)
+    assert result == expected_result
 
+def test_division_by_zero():
+    with pytest.raises(ValueError, match="Cannot divide by zero"):
+        divide_numbers(5, 0)
 
 
 
